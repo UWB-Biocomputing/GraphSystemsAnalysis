fccf18522b8ca91979fb4472e4545e0ac3dcb0c3
Vu T. Tieu <50315116+vttieu@users.noreply.github.com>
2022-02-04 07:10:01 -0800

    Documentation for getBinnedBurstInfo and getBinnedBurstSpikes
    Cleaning up documentation based on guidelines found here https://www.ee.columbia.edu/~marios/matlab/MatlabStyle1p5.pdf

--- Bursts/matlab/getBinnedBurstInfo.m
+++ Bursts/matlab/getBinnedBurstInfo.m
@@ -1,7 +1,7 @@
 %GETBINNEDBURSTINFO Identify bursts using binned data and save burst info
-%Read BrainGrid result (.h5) and get spikesHistory (spike count for every 
+%Read BrainGrid result (.h5) and get spikesHistory (spike totalSpikeCount for every 
 %10ms bin). Identify burst using burst threshold of 0.5 spikes/sec/neuron,
-%extract burst information (start/end time, count, width, size, etc)
+%extract burst information (startBinNum/end time, totalSpikeCount, width, size, etc)
 % 
 %   Syntax: getBinnedBurstInfo(h5dir)
 %   
@@ -16,36 +16,37 @@
 % Author:   Jewel Y. Lee (jewel.yh.lee@gmail.com)
 % Last updated: 11/06/2018
 function getBinnedBurstInfo(h5dir)
-% h5dir = '/CSSDIV/research/biocomputing/data/tR_1.9--fE_0.98'
+% h5dir = '/CSSDIV/research/biocomputing/data/tR_1.0--fE_0.90'        % this line is used for testing
 nNeurons = length(h5read([h5dir '.h5'], '/neuronTypes')); 
-spikeHistory = double(h5read([h5dir '.h5'], '/spikesHistory'));
-spikeHistory10msBin = spikeHistory/nNeurons;                        % spikes/neuron per bin                        
+spikesPerBin = double(h5read([h5dir '.h5'], '/spikesHistory'));
+spikesPerNeuronPer10msBin = spikesPerBin/nNeurons;                  % spikes/neuron per bin                        
 % In Kawasaki, F. & Stiber, M. (2014): 
 %   - 0.5 spikes/sec/neuron as a threshold for burst detection
 %   - for 10ms (0.01s) bin, burst threshold = 0.5x0.01 = 0.005
-burstThreashold = 0.005;                                            % burst threashold
-binsAboveThreshold = find(spikeHistory10msBin >= burstThreashold);  % find index of bins above threshold
+adjustedBurstThreshold = 0.005;                                                    % burst threashold adjusted for 10ms Bin
+binsAboveThreshold = find(spikesPerNeuronPer10msBin >= adjustedBurstThreshold);    % find index of bins above threshold
 % find burst boundaries, if the bins above the threshold is not next to each other 
 % then get their indexes
-burstBoundaries = find(diff(binsAboveThreshold) > 1);
+burstBoundaries = find(diff(binsAboveThreshold) > 1);               % contain indexes of bins above the threshold
 nBursts = length(burstBoundaries)+1;                                % number of bursts detected
 burstBoundaries = [0; burstBoundaries];                             % boundary condition
 burstBoundaries = [burstBoundaries; length(binsAboveThreshold)];    % boundary condition
 previousPeak = 0;
+
 % Output file
 fid = fopen([h5dir '/allBinnedBurstInfo.csv'], 'w') ;         
 fprintf(fid, ['ID,startBin#,endBin#,width(bins),totalSpikeCount,'... 
               'peakBin,peakHeight(spikes),Interval(bins)\n']);
 for iBurst = 1:nBursts
-    Burst.start = binsAboveThreshold(burstBoundaries(iBurst)+1);    % start bin number
-    Burst.ended = binsAboveThreshold(burstBoundaries(iBurst+1));    % end bin number
-    Burst.width = (Burst.ended-Burst.start)+1;                      % burst width in bins 
-    Burst.count = sum(spikeHistory(Burst.start:Burst.ended));       % total spike count
-    [Burst.height, temp] = max(spikeHistory(Burst.start:Burst.ended));    % height of the peak
-    Burst.peak = Burst.start+temp-1;            % peak bin (highest spike count)                
-    Burst.interval = Burst.peak-previousPeak;      % burst interval in bins
+    Burst.startBinNum = binsAboveThreshold(burstBoundaries(iBurst)+1);      % start bin number
+    Burst.endBinNum = binsAboveThreshold(burstBoundaries(iBurst+1));        % end bin number
+    Burst.width = (Burst.endBinNum - Burst.startBinNum)+1;                  % burst width in bins 
+    Burst.totalSpikeCount = sum(spikesPerBin(Burst.startBinNum:Burst.endBinNum));       % total spike totalSpikeCount
+    [Burst.peakHeight, temp] = max(spikesPerBin(Burst.startBinNum:Burst.endBinNum));    % height of the peak
+    Burst.peakBin = Burst.startBinNum+temp-1;                               % peak bin (highest spike Count)                
+    Burst.interval = Burst.peakBin-previousPeak;                            % burst interval in bins
     fprintf(fid, '%d,%d,%d,%d,%d,%d,%d,%d\n', ...
-            iBurst,Burst.start,Burst.ended,Burst.width,Burst.count,Burst.peak,Burst.height,Burst.interval);   
-    previousPeak = Burst.peak;
+            iBurst,Burst.startBinNum,Burst.endBinNum,Burst.width,Burst.totalSpikeCount,Burst.peakBin,Burst.peakHeight,Burst.interval);   
+    previousPeak = Burst.peakBin;
 end
 fclose(fid);
