973fa71ab1fa18674297105c1152cab8a95cb346
Vu T. Tieu <50315116+vttieu@users.noreply.github.com>
2022-02-11 01:50:25 -0800

    more documentation for getBinnedBurstInfo based on pull request feedback
    - added details on how the output is calculated
    - added details on what is being read as input
    - changed h5read on neuronTypes to using h5info as only the size of neuronTypes is needed to get the number of neurons
    - changed output from using struct to regular vector array

--- Bursts/matlab/getBinnedBurstInfo.m
+++ Bursts/matlab/getBinnedBurstInfo.m
@@ -1,31 +1,63 @@
-%GETBINNEDBURSTINFO Identify bursts using binned data and save burst info
-%Read BrainGrid result (.h5) and get spikesHistory (spike totalSpikeCount for every 
-%10ms bin). Identify burst using burst threshold of 0.5 spikes/sec/neuron,
-%extract burst information (startBinNum/end time, totalSpikeCount, width, size, etc)
+% GETBINNEDBURSTINFO Identify bursts using binned data and save burst info
+%
+% This function reads the BrainGrid result (.h5 file), specifically
+%   spikesHistory (contains spike Count for every 10ms bin; it is also the name of the dataset being read), 
+%   identify burst using burst threshold of 0.5 spikes/sec/neuron,
+%   calculate burst information from those bins above the threshold,
+%   and store them in allBinnedBurstInfo.csv.
+%
+% List below are those burst information and how they are calculated:
+%   ID                  - the burst ID
+%   startBin#           - The starting bin number is the bin that is above the threshold.
+%   endBin#             - The ending bin number is the next bin that is above the threshold.
+%   width(bins)         - With the starting/ending bin number, the difference is calculated as the width 
+%                         representing the number of bins in between each burst above the threshold.
+%   totalSpikeCount     - the sum of all the spikesPerBin between the starting and ending bin number
+%   peakBin             - startBin# + peakHeightIndex(index of the bin with the peakHeight) + 1
+%   peakHeight(spikes)  - calculated by finding the bin with the highest spike count between each burst
+%   Interval(bins)      - difference between current peakHeight and previous peakHeight
+%                       - previous peakHeight is initialized as 0
+% 
 % 
 %   Syntax: getBinnedBurstInfo(h5dir)
 %   
 %   Input:  
-%   h5dir  - BrainGrid simulation file (e.g. tR_1.0--fE_0.90_10000)
-%   the entire path is required for example
-%   '/CSSDIV/research/biocomputing/data/tR_1.9--fE_0.98'
+%   h5dir   -   BrainGrid simulation file (e.g. tR_1.0--fE_0.90_10000)
+%               the entire path is required for example
+%               '/CSSDIV/research/biocomputing/data/tR_1.9--fE_0.98'
 %
 %   Output:  
 %   <binnedBurstInfo.csv> - burst metadata
-
+%   the columns of the csv are named in the order of this list
+%   ID, startBin#, endBin#, width(bins), totalSpikeCount, peakBin, peakHeight(spikes), Interval(bins)
+%
 % Author:   Jewel Y. Lee (jewel.yh.lee@gmail.com)
-% Last updated: 02/04/2022
+% Last updated: 02/10/2022 added Documentation, cleaned redundant code
 % Last updated by: Vu T. Tieu (vttieu1995@gmail.com)
+
 function getBinnedBurstInfo(h5dir)
-% h5dir = '/CSSDIV/research/biocomputing/data/tR_1.0--fE_0.90'        % this line is used for testing
-nNeurons = length(h5read([h5dir '.h5'], '/neuronTypes')); 
+% line below is used for testing
+% h5dir = '/CSSDIV/research/biocomputing/data/tR_1.0--fE_0.90'        
+
+% The h5info call below for neuronTypesFileInformation will get file information like:
+%   Filename    - contains the filepath; it would be the same as h5dir
+%   Name        - in this case is neuronTypes, the name of the field you want
+%   Datatype    - array of structures describing datatypes stored in neuronTypes
+%               - this struct has Name, Class, Type, Size, Attributes as its fields
+%   Dataspace   - array of structures describing the size of the dataset
+%               - this struct has Size, MaxSize, Type as its fields
+neuronTypesFileInformation = h5info([h5dir '.h5'],'/neuronTypes');
+
+nNeurons = neuronTypesFileInformation.Dataspace.Size;               % gets size of the array neuronTypes
+% Assume each bin is 10ms
 spikesPerBin = double(h5read([h5dir '.h5'], '/spikesHistory'));
-spikesPerNeuronPer10msBin = spikesPerBin/nNeurons;                  % spikes/neuron per bin                        
+spikesPerNeuronPerBin = spikesPerBin/nNeurons;                      % spikes/neuron per bin 
+
 % In Kawasaki, F. & Stiber, M. (2014): 
 %   - 0.5 spikes/sec/neuron as a threshold for burst detection
 %   - for 10ms (0.01s) bin, burst threshold = 0.5x0.01 = 0.005
-adjustedBurstThreshold = 0.005;                                                    % burst threashold adjusted for 10ms Bin
-binsAboveThreshold = find(spikesPerNeuronPer10msBin >= adjustedBurstThreshold);    % find index of bins above threshold
+adjustedBurstThreshold = 0.005;                                                % burst threashold adjusted for 10ms Bin
+binsAboveThreshold = find(spikesPerNeuronPerBin >= adjustedBurstThreshold);    % find index of bins above threshold
 % find burst boundaries, if the bins above the threshold is not next to each other 
 % then get their indexes
 burstBoundaries = find(diff(binsAboveThreshold) > 1);               % contain indexes of bins above the threshold
@@ -39,15 +71,15 @@ fid = fopen([h5dir '/allBinnedBurstInfo.csv'], 'w') ;
 fprintf(fid, ['ID,startBin#,endBin#,width(bins),totalSpikeCount,'... 
               'peakBin,peakHeight(spikes),Interval(bins)\n']);
 for iBurst = 1:nBursts
-    Burst.startBinNum = binsAboveThreshold(burstBoundaries(iBurst)+1);      % start bin number
-    Burst.endBinNum = binsAboveThreshold(burstBoundaries(iBurst+1));        % end bin number
-    Burst.width = (Burst.endBinNum - Burst.startBinNum)+1;                  % burst width in bins 
-    Burst.totalSpikeCount = sum(spikesPerBin(Burst.startBinNum:Burst.endBinNum));       % total spike totalSpikeCount
-    [Burst.peakHeight, temp] = max(spikesPerBin(Burst.startBinNum:Burst.endBinNum));    % height of the peak
-    Burst.peakBin = Burst.startBinNum+temp-1;                               % peak bin (highest spike Count)                
-    Burst.interval = Burst.peakBin-previousPeak;                            % burst interval in bins
+    startBinNum = binsAboveThreshold(burstBoundaries(iBurst)+1);        % start bin number
+    endBinNum = binsAboveThreshold(burstBoundaries(iBurst+1));          % end bin number
+    width = (endBinNum - startBinNum)+1;                                % burst width in bins 
+    totalSpikeCount = sum(spikesPerBin(startBinNum:endBinNum));         % total spike count
+    [peakHeight, peakHeightIndex] = max(spikesPerBin(startBinNum:endBinNum));      % height of the peak
+    peakBin = startBinNum+peakHeightIndex-1;                                       % peak bin (highest spike Count)                
+    burstPeakInterval = peakBin-previousPeak;                           % burst interval in bins
     fprintf(fid, '%d,%d,%d,%d,%d,%d,%d,%d\n', ...
-            iBurst,Burst.startBinNum,Burst.endBinNum,Burst.width,Burst.totalSpikeCount,Burst.peakBin,Burst.peakHeight,Burst.interval);   
-    previousPeak = Burst.peakBin;
+            iBurst,startBinNum,endBinNum,width,totalSpikeCount,peakBin,peakHeight,burstPeakInterval);   
+    previousPeak = peakBin;
 end
 fclose(fid);
