1b3f336101547f2bca51f4845b46cdcf46cfa86f
jardiamj <1088732+jardiamj@users.noreply.github.com>
2024-03-11 23:45:24 -0700

    Added doc comments to readXmlFile and removed non-reusable script

--- ESCS/readXmlFile.m
+++ ESCS/readXmlFile.m
@@ -1,6 +1,15 @@
-
 function simOutput = readXmlFile(fileName)
-
+% readXmlFile.m Reads an XML ESCS simulation output file into a struct.
+% The XML file format must conform to the one in the "test-data" directory.
+% This is the format used in Graphitti's v0.9.2 release.
+%
+% Syntax: simOutput = readXmlFile("test-data/test-small-911-out.xml")
+%
+% Inputs:
+%   fileName - Name of the simulation output file
+%
+% Outputs:
+%   simOutput - struct that contains the ESCS simulation output
     S = readstruct(fileName);
     
     simOutput = struct;
